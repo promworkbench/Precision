3ca5cf75e00d77e6a311f3b56b53d1f9c201a30f
hmwverbeek <h.m.w.verbeek@tue.nl>
2017-05-19 10:35:30 +0000

    Added HMTL output for Event-based Precision.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/Precision/Trunk@35474 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/precision/models/EventBasedPrecision.java
+++ src/org/processmining/precision/models/EventBasedPrecision.java
@@ -1,6 +1,8 @@
 package org.processmining.precision.models;
 
-public class EventBasedPrecision {
+import org.processmining.framework.util.HTMLToString;
+
+public class EventBasedPrecision implements HTMLToString {
 
 	private long nofEvents;
 	private double sumPrecision;
@@ -28,6 +30,24 @@ public class EventBasedPrecision {
 
 	public void addNofEvents(long nofEvents) {
 		this.nofEvents += nofEvents;
+	}
+
+	public String toHTMLString(boolean includeHTMLTags) {
+		StringBuffer buf = new StringBuffer();
+		if (includeHTMLTags) {
+			buf.append("<html>");
+		}
+		buf.append("<h1>Event-based Precision</h1>");
+		buf.append("<p>" + getPrecision() + "</p>");
+		buf.append("<h2>Number of Events (<i>E</i>)</h2>");
+		buf.append("<p>" + nofEvents + "</p>");
+		buf.append("<h2>Sum of Precision Fractions (&sum;<sub><i>e</i> &isin; <i>E</i></sub><i>en<sub>L</sub></i>(<i>e</i>)/<i>en<sub>M</sub></i>(<i>e</i>))</h2>");
+		buf.append("<p>" + sumPrecision + "</p>");
+		buf.append("<hr/>");
+		if (includeHTMLTags) {
+			buf.append("</html>");
+		}
+		return buf.toString();
 	}
 	
 	
