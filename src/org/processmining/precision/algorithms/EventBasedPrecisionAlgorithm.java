03af7377cb1c41ddd8f69e9b6cc951d550c38de4
hmwverbeek <h.m.w.verbeek@tue.nl>
2017-05-19 09:47:30 +0000

    Added comments.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/Precision/Trunk@35473 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/precision/algorithms/EventBasedPrecisionAlgorithm.java
+++ src/org/processmining/precision/algorithms/EventBasedPrecisionAlgorithm.java
@@ -21,34 +21,77 @@ import org.processmining.precision.parameters.EventBasedPrecisionParameters;
 
 public class EventBasedPrecisionAlgorithm {
 
+	/*
+	 * Mapping from #hist(e) to enL(e)
+	 */
 	private Map<List<String>, Set<String>> enL;
+	/*
+	 * Mapping from #hist(e) to enM(e)
+	 */
 	private Map<List<String>, Set<String>> enM;
+	/*
+	 * Mapping from state in the net to set of enabled activities. Used as cache.
+	 */
+	private Map<Marking, Set<String>> enA;
 
 	public EventBasedPrecisionAlgorithm() {
+		/*
+		 * Initialize.
+		 */
 		enL = new HashMap<List<String>, Set<String>>();
 		enM = new HashMap<List<String>, Set<String>>();
+		enA = new HashMap<Marking, Set<String>>();
 	}
 
+	/*
+	 * Get the event-based precision given the alignments and the net (apn). ssumption is that the net
+	 * was used to create the alignments (the transitions in the alignments should be transitions from
+	 * this net).
+	 */
 	public EventBasedPrecision apply(PluginContext context, PNRepResult alignments, AcceptingPetriNet apn,
 			EventBasedPrecisionParameters parameters) throws IllegalTransitionException {
 		EventBasedPrecision precision = new EventBasedPrecision();
+		/*
+		 * First, construct enL en enM.
+		 */
 		for (SyncReplayResult alignment : alignments) {
 			apply(context, alignment, apn, parameters);
 		}
+		/*
+		 * Second, compute precision based on constructed enL en enM. 
+		 */
 		for (SyncReplayResult alignment : alignments) {
-			apply(context, alignment, apn, precision, parameters);
+			apply(context, alignment, precision);
 		}
+		/*
+		 * Return precision.
+		 */
 		return precision;
 	}
 
+	/*
+	 * Extend enL en enM with the given alignment.
+	 */
 	private void apply(PluginContext context, SyncReplayResult alignment, AcceptingPetriNet apn,
 			EventBasedPrecisionParameters parameters) throws IllegalTransitionException {
+		/*
+		 * Current state in the net.
+		 */
 		Marking state = apn.getInitialMarking();
+		/*
+		 * Current history in alignment. Initially empty.
+		 */
 		List<String> hist = new ArrayList<String>();
 
 		for (int i = 0; i < alignment.getStepTypes().size(); i++) {
+			/*
+			 * Get current step type an dnode instance.
+			 */
 			StepTypes stepType = alignment.getStepTypes().get(i);
 			Object nodeInstance = alignment.getNodeInstance().get(i);
+			/*
+			 * Make sure history is in enL and enM.
+			 */
 			if (!enL.containsKey(hist)) {
 				enL.put(new ArrayList<String>(hist), new HashSet<String>());
 				enM.put(new ArrayList<String>(hist), new HashSet<String>());
@@ -57,36 +100,63 @@ public class EventBasedPrecisionAlgorithm {
 			switch (stepType) {
 				case MREAL : {
 					Transition transition = (Transition) nodeInstance;
+					/*
+					 * From the current history, the log can do the activity associated with this transition.
+					 */
 					enL.get(hist).add(transition.getLabel());
+					/*
+					 * Find all enabled activities from the current state.
+					 */
 					enM.get(hist).addAll(getEnabledActivities(state, new HashSet<Transition>(), parameters));
+					/*
+					 * Update state by executing transition.
+					 */
 					parameters.getSemantics().setCurrentState(state);
 					parameters.getSemantics().executeExecutableTransition(transition);
 					state = new Marking(parameters.getSemantics().getCurrentState());
-//					hist.add(transition.getLabel());
 					break;
 				}
 				case LMGOOD : {
 					Transition transition = (Transition) nodeInstance;
+					/*
+					 * From the current history, the log can do the activity associated with this transition.
+					 */
 					enL.get(hist).add(transition.getLabel());
+					/*
+					 * Find all enabled activities from the current state.
+					 */
 					enM.get(hist).addAll(getEnabledActivities(state, new HashSet<Transition>(), parameters));
+					/*
+					 * Update state by executing transition.
+					 */
 					parameters.getSemantics().setCurrentState(state);
 					parameters.getSemantics().executeExecutableTransition(transition);
 					state = new Marking(parameters.getSemantics().getCurrentState());
+					/*
+					 * Update history.
+					 */
 					hist.add(transition.getLabel());
 					break;
 				}
 				case MINVI : {
 					Transition transition = (Transition) nodeInstance;
+					/*
+					 * Find all enabled activities from the current state.
+					 */
 					enM.get(hist).addAll(getEnabledActivities(state, new HashSet<Transition>(), parameters));
+					/*
+					 * Update state by executing transition.
+					 */
 					parameters.getSemantics().setCurrentState(state);
 					parameters.getSemantics().executeExecutableTransition(transition);
 					state = new Marking(parameters.getSemantics().getCurrentState());
 					break;
 				}
 				case L : {
+					/*
+					 * Update history.
+					 */
 					XEventClass activity = (XEventClass) nodeInstance;
-					//					enL.get(hist).add(activity.getId());
-					//					enM.get(hist).addAll(getEnabledActivities(state, new HashSet<Transition>(), parameters));
 					hist.add(activity.getId());
 					break;
 				}
@@ -95,41 +165,110 @@ public class EventBasedPrecisionAlgorithm {
 		}
 	}
 
+	/*
+	 * Get the enabled activities form the given state.
+	 */
 	private Set<String> getEnabledActivities(Marking state, Set<Transition> transitions,
 			EventBasedPrecisionParameters parameters) throws IllegalTransitionException {
+		/*
+		 * Check cache.
+		 */
+		if (enA.containsKey(state)) {
+			/*
+			 * Already in cache. Do not recompute.
+			 */
+			return enA.get(state);
+		}
+		/*
+		 * Not in cache. Compute. Start with empty set of activities.
+		 */
 		Set<String> activities = new HashSet<String>();
+		/*
+		 * Set current state.
+		 */
 		parameters.getSemantics().setCurrentState(state);
-		Set<Transition> executableTransitions = new HashSet<Transition>(parameters.getSemantics().getExecutableTransitions());
+		/*
+		 * Get transitions enabled in current state.
+		 */
+		Set<Transition> executableTransitions = new HashSet<Transition>(parameters.getSemantics()
+				.getExecutableTransitions());
+		/*
+		 * Execute these transitions one after the other.
+		 */
 		for (Transition transition : executableTransitions) {
+			/*
+			 * Cycle check
+			 */
 			if (!transitions.contains(transitions)) {
 				transitions.add(transition);
 				if (transition.isInvisible()) {
+					/*
+					 * Silent transition. Execute and look further.
+					 */
 					parameters.getSemantics().executeExecutableTransition(transition);
+					/*
+					 * Add all activities enabled from the state after this silent transition has been executed.
+					 */
 					activities.addAll(getEnabledActivities(new Marking(parameters.getSemantics().getCurrentState()),
 							transitions, parameters));
+					/*
+					 * Reset the current state for the next transition.
+					 */
 					parameters.getSemantics().setCurrentState(state);
 				} else {
+					/*
+					 * Visible transition. Add corresponding activity.
+					 */
 					activities.add(transition.getLabel());
 				}
 			}
 		}
 		System.out.println("[EventBasedPrecisionAlgorithm] State = " + state + ", activities = " + activities);
+		/*
+		 * Cache the result.
+		 */
+		enA.put(state, activities);
+		/*
+		 * Return the result.
+		 */
 		return activities;
 	}
 
-	private void apply(PluginContext context, SyncReplayResult alignment, AcceptingPetriNet apn,
-			EventBasedPrecision precision, EventBasedPrecisionParameters parameters) throws IllegalTransitionException {
-		Marking state = apn.getInitialMarking();
+	/*
+	 * Given enL and enM, extend the precision with the given alignment.
+	 */
+	private void apply(PluginContext context, SyncReplayResult alignment, EventBasedPrecision precision) {
+		/*
+		 * History is initally empty.
+		 */
 		List<String> hist = new ArrayList<String>();
+		/*
+		 * Alignment corresponds to this many traces in event log.
+		 */
 		int n = alignment.getTraceIndex().size();
 
 		for (int i = 0; i < alignment.getStepTypes().size(); i++) {
+			/*
+			 * Get step type an dnode instance.
+			 */
 			StepTypes stepType = alignment.getStepTypes().get(i);
 			Object nodeInstance = alignment.getNodeInstance().get(i);
 
+			/*
+			 * Check whether we're at some event, and whether it makes sense.
+			 */
 			if ((stepType == StepTypes.L || stepType == StepTypes.LMGOOD) && enM.get(hist).size() > 0) {
+				/*
+				 * Add as amny events to the precision as there were traces corresponding to this alignment.
+				 */
 				precision.addNofEvents(n);
+				/*
+				 * Extend the precision in a similar way. By definition of the precision.
+				 */
 				precision.addSumPrecision(n * (((double) enL.get(hist).size()) / enM.get(hist).size()));
+				/*
+				 * Output mismatches, that is, if precision drops. Could be useful diagnostic information.
+				 */
 				if (!enL.get(hist).equals(enM.get(hist))) {
 					System.out.println("[EventBasedPrecisionAlgorithm] Hist = " + hist + ", enL = " + enL.get(hist)
 							+ ", enM = " + enM.get(hist));
@@ -138,28 +277,19 @@ public class EventBasedPrecisionAlgorithm {
 				}
 			}
 
+			/*
+			 * Update the history.
+			 */
 			switch (stepType) {
 				case MREAL : {
-					Transition transition = (Transition) nodeInstance;
-					parameters.getSemantics().setCurrentState(state);
-					parameters.getSemantics().executeExecutableTransition(transition);
-					state = new Marking(parameters.getSemantics().getCurrentState());
-//					hist.add(transition.getLabel());
 					break;
 				}
 				case LMGOOD : {
 					Transition transition = (Transition) nodeInstance;
-					parameters.getSemantics().setCurrentState(state);
-					parameters.getSemantics().executeExecutableTransition(transition);
-					state = new Marking(parameters.getSemantics().getCurrentState());
 					hist.add(transition.getLabel());
 					break;
 				}
 				case MINVI : {
-					Transition transition = (Transition) nodeInstance;
-					parameters.getSemantics().setCurrentState(state);
-					parameters.getSemantics().executeExecutableTransition(transition);
-					state = new Marking(parameters.getSemantics().getCurrentState());
 					break;
 				}
 				case L : {
